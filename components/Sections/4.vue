e6e9c5036e619e7b0b3c2a6bbf3cdf07832341a1
Innocent Mwatsikesimbe <imwatsi@outlook.com>
2022-03-06 01:40:10 +0200

    add polls description

--- components/Sections/4.vue
+++ components/Sections/4.vue
@@ -7,7 +7,7 @@
   >
     <CardExtended
       title="Polls"
-      description="Features you can freely add to your dApp, available on Hive Plug & Play right now."
+      description="Enabling users to capture the quantitative distribution of opinions and reach public consensus"
       background="#EFF0F9;"
       accent="#0080FF"
       image="img6.svg"
